a1bf233c3f4b248034657b1968d46297c22d1111
ezio-da-firenze <126500974+ezio-da-firenze@users.noreply.github.com>
2025-06-14 23:48:22 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,21 +1,50 @@
+// const { Sequelize } = require("sequelize");
+// require("dotenv").config();
+
+// // Connecting to a local MySQL server
+
+// // Getting the details of the database from the .env file
+// const { DB_NAME, DB_USER, DB_PASSWORD, DB_PORT } = process.env;
+
+// // Creating a sequelize object for further reference
+// const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+//     host: "localhost",
+//     dialect: "mysql",
+//     port: DB_PORT,
+//     logging: false,
+// });
+
+// if (sequelize) {
+//     console.log("DB connected");
+// }
+
+// module.exports = sequelize;
+
+
 const { Sequelize } = require("sequelize");
+
 require("dotenv").config();
 
-// Connecting to a local MySQL server
+const { DB_HOST, DB_NAME, DB_USER, DB_PASSWORD, DB_PORT } = process.env;
 
-// Getting the details of the database from the .env file
-const { DB_NAME, DB_USER, DB_PASSWORD, DB_PORT } = process.env;
-
-// Creating a sequelize object for further reference
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-    host: "localhost",
-    dialect: "mysql",
+    host: DB_HOST,
     port: DB_PORT,
+    dialect: "postgres",
     logging: false,
+    dialectOptions: {
+        ssl: { require: true, rejectUnauthorized: false },
+    },
 });
 
-if (sequelize) {
-    console.log("DB connected");
-}
+// Test the connection immediately
+(async () => {
+    try {
+        await sequelize.authenticate();
+        console.log("Connected to  Postgres database successfully...");
+    } catch (error) {
+        console.error("Unable to connect to the database:", error);
+    }
+})();
 
 module.exports = sequelize;
