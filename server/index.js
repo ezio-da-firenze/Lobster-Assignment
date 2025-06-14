310bf32853146ac9d073370d19396a2114cc64c2
ezio-da-firenze <126500974+ezio-da-firenze@users.noreply.github.com>
2025-06-15 00:42:32 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,9 +13,10 @@ const cookieParser = require("cookie-parser");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const corsOptions = {
-    origin: "http://localhost:3001",
+    origin: ["http://localhost:3001", "http://localhost:5173"],
     credentials: true, // to allow cookies
 };
+
 
 // MW
 app.use(cors(corsOptions));
